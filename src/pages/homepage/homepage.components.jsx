9c21b78bcf7f9da6bb65fc14a8527555e193c647
neil woodhouse <neilwdy@hotmail.com>
2021-04-12 20:15:05 +0100

    update our menu items to naviagate withRouter HOC

--- src/pages/homepage/homepage.components.jsx
+++ src/pages/homepage/homepage.components.jsx
@@ -4,7 +4,7 @@ import Directory from '../../components/directory/directory.component';
 import './homepage.styles.scss';
 
 const HomePage = () => (
-    <Directory />
+    <Directory  />
 );
 
 export default HomePage;
